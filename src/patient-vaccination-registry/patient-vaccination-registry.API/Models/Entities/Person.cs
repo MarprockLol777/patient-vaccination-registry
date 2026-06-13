70165c62b1b6163f9fc138e71dd4f75f442b1644
Marcos Eliú Cuevas Jiménez <68508925+MarprockLol777@users.noreply.github.com>
2026-06-12 21:12:07 -0400

    I have finally created the Person entity and its corresponding controller.

---
+++ src/patient-vaccination-registry/patient-vaccination-registry.API/Models/Entities/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace patient_vaccination_registry.API.Models.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? IdNumber { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
