9b4ebde7f5ced634f63cb10f202552ad1bd396e1
Marcos Eliú Cuevas Jiménez <68508925+MarprockLol777@users.noreply.github.com>
2026-07-18 19:35:13 -0400

    I've finally completed the Infrastructure layer
    I created a new Class Library project called Patient-Vaccination-Registry.Infrastructure.
    
    In this new project, I created three folders: Context, Core, and Repositories.
    
    Inside the Context folder, I moved the DataContext class from the Data folder, which was located in the Models folder of the Patient-Vaccination-Registry.API project. Now, the DataContext class is inside the Context folder instead of the Data folder.
    
    Inside the Core folder, I created the GenericRepository class. This class will be the parent class for all the repositories in the Repositories folder. By inheriting from GenericRepository, all repositories share the same methods, so they don't need to be implemented repeatedly in every repository.

---
+++ src/patient-vaccination-registry/patient-vaccination-registry.Infrastructure/Context/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using patient_vaccination_registry.Domain.Entities;
+
+namespace patient_vaccination_registry.Infrastructure.Context 
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions <DataContext> options): base(options)
+        { 
+        }
+
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Drive> Drives { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Vaccine> Vaccines { get; set; }
+    }
+}
---
+++ src/patient-vaccination-registry/patient-vaccination-registry.Infrastructure/Repositories/VaccineRepository.cs
@@ -0,0 +1,13 @@
+﻿using patient_vaccination_registry.Domain.Entities;
+using patient_vaccination_registry.Infrastructure.Context;
+using patient_vaccination_registry.Infrastructure.Core;
+
+namespace patient_vaccination_registry.Infrastructure.Repositories
+{
+    public class VaccineRepository : GenericRepository<Vaccine>
+    {
+        public VaccineRepository(DataContext context) : base(context)
+        {
+        }
+    }
+}
